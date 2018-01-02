93c02f91c80d44ae5cce84e4427e076941cf481c
Tauqeer <tauqeerulhassan45@gmail.com>
2018-01-02 10:32:13 +0500

    Frontend Changes

--- public/polyfills.bundle.js
+++ public/polyfills.bundle.js
@@ -33,7 +33,7 @@ var ac_polyfills =
 /******/
 /******/ 	// objects to store loaded and loading chunks
 /******/ 	var installedChunks = {
-/******/ 		20: 0
+/******/ 		21: 0
 /******/ 	};
 /******/
 /******/ 	// The require function
