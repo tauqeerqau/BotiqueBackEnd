7dd0ae9a3c069dfabdfc97632b0cfa0e866490d2
Tauqeer <tauqeerulhassan45@gmail.com>
2018-01-24 15:27:28 +0500

    Updated Live DB

--- utilities/Database.js
+++ utilities/Database.js
@@ -8,7 +8,8 @@ var Database = function Constructor() {
     
     
     Database.prototype.connectDatabase = function () {
-        var url = 'mongodb://admin:ideofuzion123@ds159926.mlab.com:59926/botiquedb';
+        //var url = 'mongodb://admin:ideofuzion123@ds159926.mlab.com:59926/botiquedb';
+        var url = 'mongodb://dbAdmin:ideofuzion123@ds135069.mlab.com:35069/botiquelive';
         mongoose.connect(url, function(err, db) {
             if (err) {
                 console.log(err);
